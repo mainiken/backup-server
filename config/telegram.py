ea062bf1102610790c216adab8ebd659f3b3aee5
MAINECODE <48281208+mainiken@users.noreply.github.com>
2025-01-26 16:55:28 +0300

    Update telegram.py

--- config/telegram.py
+++ config/telegram.py
@@ -24,36 +24,30 @@ class TelegramNotifier:
 
     def send_message(self, text):
         try:
-            self.logger.info(f"Отправка сообщения в Telegram")
             response = requests.post(
                 f'https://api.telegram.org/bot{self.token}/sendMessage',
                 data={'chat_id': self.chat_id, 'text': text}
             )
             response.raise_for_status()
-            self.logger.info("Сообщение успешно отправлено")
         except Exception as e:
             self.logger.error(f"Ошибка отправки сообщения: {str(e)}", exc_info=True)
 
-    def send_file(self, file_path):
+    def send_file(self, file_path, caption=None):
         try:
-            self.logger.info(f"Начинаю отправку файла {file_path}")
             if not os.path.exists(file_path):
                 self.logger.error(f"Файл не найден: {file_path}")
                 return
                 
             with open(file_path, 'rb') as file:
-                self.logger.info("Файл открыт, отправляю в Telegram...")
-                caption = "Backup file" if str(file_path).endswith('.tar.gz') else "Log file"
+                data = {'chat_id': self.chat_id}
+                if caption:
+                    data['caption'] = caption
+                    
                 response = requests.post(
                     f'https://api.telegram.org/bot{self.token}/sendDocument',
-                    data={
-                        'chat_id': self.chat_id,
-                        'caption': caption
-                    },
+                    data=data,
                     files={'document': file}
                 )
-                self.logger.info(f"Ответ от Telegram: {response.status_code}")
                 response.raise_for_status()
-                self.logger.info("Файл успешно отправлен")
         except Exception as e:
             self.logger.error(f"Ошибка отправки файла: {str(e)}", exc_info=True)
