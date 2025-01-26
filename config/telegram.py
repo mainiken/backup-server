3476732d77d31eb13daa8a4bbd7eed3416489f0b
MAINECODE <48281208+mainiken@users.noreply.github.com>
2025-01-26 16:36:19 +0300

    Update telegram.py

--- config/telegram.py
+++ config/telegram.py
@@ -5,7 +5,6 @@ from .settings import settings
 
 def setup_logger():
     settings.log_dir.mkdir(exist_ok=True)
-    
     logging.basicConfig(
         level=logging.INFO,
         format='%(asctime)s - %(levelname)s - %(message)s',
@@ -24,22 +23,28 @@ class TelegramNotifier:
         
     def send_message(self, text):
         try:
+            self.logger.info(f"Отправка сообщения в Telegram")
             response = requests.post(
                 f'https://api.telegram.org/bot{self.token}/sendMessage',
                 data={'chat_id': self.chat_id, 'text': text}
             )
             response.raise_for_status()
+            self.logger.info("Сообщение успешно отправлено")
         except Exception as e:
-            self.logger.error(f"Ошибка отправки сообщения в Telegram: {e}")
+            self.logger.error(f"Ошибка отправки сообщения: {str(e)}", exc_info=True)
 
     def send_file(self, file_path):
         try:
+            self.logger.info(f"Начинаю отправку файла {file_path}")
             with open(file_path, 'rb') as file:
+                self.logger.info("Файл открыт, отправляю в Telegram...")
                 response = requests.post(
                     f'https://api.telegram.org/bot{self.token}/sendDocument',
                     data={'chat_id': self.chat_id},
                     files={'document': file}
                 )
+                self.logger.info(f"Ответ от Telegram: {response.status_code}")
                 response.raise_for_status()
+                self.logger.info("Файл успешно отправлен")
         except Exception as e:
-            self.logger.error(f"Ошибка отправки файла в Telegram: {e}")
+            self.logger.error(f"Ошибка отправки файла: {str(e)}", exc_info=True)
