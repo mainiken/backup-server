77855fff63e0622734cfd5d62b1c6c44113f0c3a
MAINECODE <48281208+mainiken@users.noreply.github.com>
2025-01-26 15:37:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,14 @@
+from config.menu import BackupMenu
+from config.settings import settings
 import sys
-from src.interface.cli import parse_args
-from src.interface.menu import BackupMenu
-from src.config.settings import settings
-from src.utils.logger import setup_logger
 
 def main():
-    logger = setup_logger()
-    
-    if not settings.is_configured:
-        logger.error("Application is not configured. Please check .env file.")
+    if not all([settings.telegram_token, settings.chat_id]):
+        print("Ошибка: проверьте настройки в файле .env")
         sys.exit(1)
-    
-    args = parse_args()
-    
-    if args.run:
-        # Запуск бэкапа напрямую (для cron)
-        from src.backup.backup_manager import BackupManager
-        backup_manager = BackupManager()
-        backup_manager.run()
-    else:
-        # Запуск интерактивного меню
-        menu = BackupMenu()
-        menu.run()
+        
+    menu = BackupMenu()
+    menu.run()
 
 if __name__ == "__main__":
     main()
